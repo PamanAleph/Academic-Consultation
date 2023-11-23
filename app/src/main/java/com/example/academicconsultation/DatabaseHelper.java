3c94516e648b5687a63d07bedfd4d41914197621
RUNATISSENA <kaze.u23sena@gmail.com>
2023-11-23 15:59:00 +0700

    DBHelper

---
+++ app/src/main/java/com/example/academicconsultation/DatabaseHelper.java
@@ -0,0 +1,36 @@
+package com.example.academicconsultation;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "notes_database";
+    private static final int DATABASE_VERSION = 1;
+
+    // Table name and column names
+    public static final String TABLE_NOTES = "notes";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NOTE = "note";
+
+    // Create table query
+    private static final String CREATE_TABLE_NOTES =
+            "CREATE TABLE " + TABLE_NOTES + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_NOTE + " TEXT);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_NOTES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Handle database upgrade if needed
+    }
+}
+
---
+++ app/src/main/java/com/example/academicconsultation/NotesDataSource.java
@@ -0,0 +1,54 @@
+package com.example.academicconsultation;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class NotesDataSource {
+
+    private SQLiteDatabase database;
+    private DatabaseHelper dbHelper;
+
+    public NotesDataSource(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    // Create a new note
+    public long createNote(String note) {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COLUMN_NOTE, note);
+        return database.insert(DatabaseHelper.TABLE_NOTES, null, values);
+    }
+
+    // Read all notes
+    public Cursor getAllNotes() {
+        String[] allColumns = {DatabaseHelper.COLUMN_ID, DatabaseHelper.COLUMN_NOTE};
+        return database.query(DatabaseHelper.TABLE_NOTES, allColumns,
+                null, null, null, null, null);
+    }
+
+    // Update a note
+    public int updateNote(long id, String newNote) {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COLUMN_NOTE, newNote);
+        return database.update(DatabaseHelper.TABLE_NOTES, values,
+                DatabaseHelper.COLUMN_ID + " = " + id, null);
+    }
+
+    // Delete a note
+    public int deleteNote(long id) {
+        return database.delete(DatabaseHelper.TABLE_NOTES,
+                DatabaseHelper.COLUMN_ID + " = " + id, null);
+    }
+}
+
