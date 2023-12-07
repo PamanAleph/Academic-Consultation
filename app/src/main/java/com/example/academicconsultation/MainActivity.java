0421153ff1da1802cc77aca35b900c7a48babeff
RUNATISSENA <kaze.u23sena@gmail.com>
2023-12-07 15:30:04 +0700

    DBHelper

--- app/src/main/java/com/example/academicconsultation/MainActivity.java
+++ app/src/main/java/com/example/academicconsultation/MainActivity.java
@@ -9,18 +9,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonRegister;
+    Button buttonRegister, buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonRegister = findViewById(R.id.buttonRegister);
+        buttonLogin = findViewById(R.id.buttonLogin);
 
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, RegisterPage.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
